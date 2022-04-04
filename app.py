ba96146996a5a8ad0790a91e35df12ce38a90789
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-04-04 10:43:57 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+podname = os.uname()[1]
+
+@app.route("/")
+def index():
+    return " Container EDU | POD Working : " + podname + " | v=1\n"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
